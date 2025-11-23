3aef153410c5abf87b822068dcff16f94e5b9c14
Bakar7725 <abug4201@gmail.com>
2025-11-23 12:17:04 +0500

    Final v2.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,25 @@
 import { useState } from 'react';
 import Navbar from './components/Navbar';
-import Content1 from './components/content_1';
-import SigninModal from './components/Signin';
 import Content from './components/content_1'; 
 import Content2 from './components/content_1.1';
 import Content3 from './components/content_1.2';
 import Content4 from './components/content_1.3';
+import Signin from './components/signin';
 import DonorPatient from './components/DonorPatient';
 import Patient from './components/Patients';
 import Contact from './components/Contact';
 import About from './components/About';
 import SignupModal from './components/Signup';
-import Content1_1 from './components/content_1_1';
-import Content1_2 from './components/content_1_2';
-import Patient_view from './components/Patient_view';
 
 function App() {
   const [showSignin, setShowSignin] = useState(false);
-  const [showSignup, setShowSignup] = useState(false);
-  const [showRegister, setShowRegister] = useState(false);
+  const [showRegister, setShowRegister] = useState(false); // single source of truth
   const [showDonors, setShowDOnors] = useState(false);
   const [showPatients, setShowPatients] = useState(false);
 
   function ShowSigninForm() {
     setShowSignin(true);
-    setShowSignup(false);
   }
-
   function hideSigninForm() {
     setShowSignin(false);
   }
@@ -34,7 +27,6 @@ function App() {
   function C() {
     setShowRegister(true);
   }
-
   function D() {
     setShowRegister(false);
   }
@@ -42,7 +34,6 @@ function App() {
   function onShowDonors() {
     setShowDOnors(true);
   }
-
   function onHideDonors() {
     setShowDOnors(false);
   }
@@ -50,45 +41,35 @@ function App() {
   function onShowPatients() {
     setShowPatients(true);
   }
-
   function onHidePatients() {
     setShowPatients(false);
   }
+
+const [showSignup, setShowSignup] = useState(false);
+
+function openSignup() {
+  setShowSignup(true);
+}
+
+function closeSignup() {
+  setShowSignup(false);
+}
 
   return (
     <div>
       <Navbar 
         onShowSignin={ShowSigninForm}
-        onShowSignup={() => { setShowSignup(true); setShowSignin(false); }}
         ShowDonors={onShowDonors}
         showPatients={onShowPatients}
-        openSignup={() => setShowSignup(true)}
+        openSignup={openSignup}
       />
 
-      {/* Show SigninModal when showSignin is true */}
-      {showSignin && <SigninModal onClose={() => setShowSignin(false)} />}
-      {showSignup && (
-        <SignupModal 
-          onClose={() => setShowSignup(false)} 
-          onShowSignin={() => {
-            setShowSignin(true);
-            setShowSignup(false);
-          }}
-        />
-      )}
-
-      {/* Your other content */}
-      <Content1 />
-      <Content1_1 />
-      <Content1_2 />
-      <section id="donors-section">
-        <Patient_view />
-      </section>
-
       {/* Modals */}
-      {showDonors && <Content4 onClose={() => setShowDOnors(false)} />}
-      {showPatients && <Patient onClose={() => setShowPatients(false)} />}
-      
+      {showSignin && <Signin onClose={hideSigninForm} />}
+      {showDonors && <Content4 onClose={onHideDonors} />}
+      {showPatients && <Patient onClose={onHidePatients} />}
+      {showSignup && <SignupModal onClose={closeSignup} onShowSignin={ShowSigninForm} />}
+
       {/* Page Sections */}
       <div id="home"><Content /></div>
       <div id="content2"><Content2 /></div>
@@ -98,6 +79,7 @@ function App() {
       </div>
       <div id="contact"><Contact /></div>
       <div id="about"><About /></div>
+      {/* Removed duplicated <Patient /> from here */}
     </div>
   );
 }
